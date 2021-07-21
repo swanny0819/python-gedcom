5baca6c7d5ed77ef34bb0a2f5f8161025462f476
Patrick Swanson <patswanson@gmail.com>
2021-07-21 17:35:29 -0500

    Refactor IndividualElement test file.
    * Remove unnecessary initialization test for IndividualElement
    * Refactor only remaining test for IndividualElement to ensure we got the right results.

--- tests/element/test_individual.py
+++ tests/element/test_individual.py
@@ -1,21 +1,18 @@
-from gedcom.element.element import Element
+import unittest
+
 from gedcom.element.individual import IndividualElement
-import gedcom.tags
 
 
-def test_initialization():
-    individual_element = IndividualElement(level=-1, pointer="", tag=gedcom.tags.GEDCOM_TAG_INDIVIDUAL, value="")
-    assert isinstance(individual_element, Element)
-    assert isinstance(individual_element, IndividualElement)
+class TestIndividualElement(unittest.TestCase):
+    def test_get_all_names__should_be_able_to_match_multiple_names_but_not_non_names(self):
+        element = IndividualElement(level=0, pointer="@I5@", tag="INDI", value="")
+        element.new_child_element(tag="NAME", value="First /Last/")
+        element.new_child_element(tag="SEX", value="M")
+        birth = element.new_child_element(tag="BIRT", value="")
+        birth.new_child_element(tag="DATE", value="1 JAN 1900")
+        element.new_child_element(tag="NAME", value="Second /Surname/")
 
-
-def test_get_all_names():
-    element = IndividualElement(level=0, pointer="@I5@", tag="INDI", value="")
-    element.new_child_element(tag="NAME", value="First /Last/")
-    element.new_child_element(tag="SEX", value="M")
-    birth = element.new_child_element(tag="BIRT", value="")
-    birth.new_child_element(tag="DATE", value="1 JAN 1900")
-    element.new_child_element(tag="NAME", value="Second /Surname/")
-
-    all_names = element.get_all_names()
-    assert len(all_names) == 2
+        all_names = element.get_all_names()
+        self.assertEqual(2, len(all_names))
+        self.assertEqual('First /Last/', all_names[0])
+        self.assertEqual('Second /Surname/', all_names[1])
