30a4095e74e720faac4c0c3386108333625ee88a
Patrick Swanson <patswanson@gmail.com>
2021-07-21 12:43:18 -0500

    Convert parser tests to use unittest's assertEqual instead of python's assert.
    * Now when one of these tests fails, we'll get an error with both values, rather than 'False'.
    * Also removed two tests that were not meaningful: one stub that did nothing, and another than only tested python's ability to instantiate a class.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,57 +1,51 @@
+import unittest
+
 from gedcom.element.individual import IndividualElement
 from gedcom.element.root import RootElement
 from gedcom.parser import Parser
 
 
-def test_initialization():
-    parser = Parser()
-    assert isinstance(parser, Parser)
+class TestParser(unittest.TestCase):
+    def test_invalidate_cache(self):
+        parser = Parser()
+        parser.parse_file('../tests/files/Musterstammbaum.ged')
 
+        self.assertEqual(32, len(parser.get_element_dictionary()))
+        self.assertEqual(396, len(parser.get_element_list()))
 
-def test_invalidate_cache():
-    parser = Parser()
-    parser.parse_file('tests/files/Musterstammbaum.ged')
+        parser.invalidate_cache()
 
-    assert len(parser.get_element_list()) == 396
-    assert len(parser.get_element_dictionary()) == 32
+        self.assertEqual(32, len(parser.get_element_dictionary()))
+        self.assertEqual(396, len(parser.get_element_list()))
 
-    parser.invalidate_cache()
+    def test_get_root_element(self):
+        parser = Parser()
+        self.assertTrue(isinstance(parser.get_root_element(), RootElement))
 
-    assert len(parser.get_element_list()) == 396
-    assert len(parser.get_element_dictionary()) == 32
+    def test_parse_file(self):
+        parser = Parser()
+        self.assertEqual(0, len(parser.get_root_child_elements()))
 
+        parser.parse_file('../tests/files/Musterstammbaum.ged')
 
-def test_get_root_element():
-    parser = Parser()
-    assert isinstance(parser.get_root_element(), RootElement)
+        self.assertEqual(34, len(parser.get_root_child_elements()))
 
+        individuals_in_root_child_elements = 0
+        individuals_in_element_list = 0
 
-def test_parse_file():
-    parser = Parser()
+        for element in parser.get_root_child_elements():
+            if isinstance(element, IndividualElement):
+                individuals_in_root_child_elements += 1
 
-    assert len(parser.get_root_child_elements()) == 0
+        for element in parser.get_element_list():
+            if isinstance(element, IndividualElement):
+                individuals_in_element_list += 1
 
-    parser.parse_file('tests/files/Musterstammbaum.ged')
+        self.assertEqual(20, individuals_in_root_child_elements)
+        self.assertEqual(20, individuals_in_element_list)
 
-    assert len(parser.get_root_child_elements()) == 34
-
-    individuals_in_root_child_elements = 0
-    individuals_in_element_list = 0
-
-    for element in parser.get_root_child_elements():
-        if isinstance(element, IndividualElement):
-            individuals_in_root_child_elements += 1
-
-    for element in parser.get_element_list():
-        if isinstance(element, IndividualElement):
-            individuals_in_element_list += 1
-
-    assert individuals_in_root_child_elements == 20
-    assert individuals_in_element_list == 20
-
-
-def test_parse_from_string():
-    case_1 = """0 @I5@ INDI
+    def test_parse_from_string(self):
+        case_1 = """0 @I5@ INDI
 1 NAME First /Last/
 1 SEX M
 1 BIRT
@@ -61,19 +55,19 @@ def test_parse_from_string():
 4 LATI N47.680663
 4 LONG W122.234319
 """
-    gedcom_parser = Parser()
-    gedcom_parser.parse([(a + '\n').encode('utf-8-sig') for a in case_1.splitlines()])
-    element_1 = gedcom_parser.get_root_child_elements()[0]
-    assert isinstance(element_1, IndividualElement)
-    assert element_1.get_tag() == 'INDI'
-    assert element_1.get_pointer() == '@I5@'
-    element_1_children = element_1.get_child_elements()
-    assert len(element_1_children) == 3
-    assert element_1_children[0].get_tag() == 'NAME'
-    assert element_1_children[1].get_tag() == 'SEX'
-    assert element_1_children[2].get_tag() == 'BIRT'
+        gedcom_parser = Parser()
+        gedcom_parser.parse([(a + '\n').encode('utf-8-sig') for a in case_1.splitlines()])
+        element_1 = gedcom_parser.get_root_child_elements()[0]
+        self.assertTrue(isinstance(element_1, IndividualElement))
+        self.assertEqual('INDI', element_1.get_tag())
+        self.assertEqual('@I5@', element_1.get_pointer())
+        element_1_children = element_1.get_child_elements()
+        self.assertEqual(3, len(element_1_children))
+        self.assertEqual('NAME', element_1_children[0].get_tag())
+        self.assertEqual('SEX', element_1_children[1].get_tag())
+        self.assertEqual('BIRT', element_1_children[2].get_tag())
 
-    case_2 = """0 @F28@ FAM
+        case_2 = """0 @F28@ FAM
 1 HUSB @I80@
 1 WIFE @I81@
 1 CHIL @I9@
@@ -86,18 +80,13 @@ def test_parse_from_string():
 2 _FREL Natural
 2 _MREL Natural
 """
-    gedcom_parser.parse([(a + '\n').encode('utf-8-sig') for a in case_2.splitlines()])
-    element_2 = gedcom_parser.get_root_child_elements()[0]
-    assert element_2.get_tag() == 'FAM'
-    assert element_2.get_pointer() == '@F28@'
-    element_2_children = element_2.get_child_elements()
-    assert len(element_2_children) == 5
-    assert element_2_children[0].get_tag() == 'HUSB'
-    assert element_2_children[1].get_tag() == 'WIFE'
-    assert element_2_children[2].get_tag() == 'CHIL'
-    assert element_2_children[3].get_value() == '@I84@'
-
-
-def test___parse_line():
-    # @TODO Add appropriate testing cases
-    pass
+        gedcom_parser.parse([(a + '\n').encode('utf-8-sig') for a in case_2.splitlines()])
+        element_2 = gedcom_parser.get_root_child_elements()[0]
+        self.assertEqual('FAM', element_2.get_tag())
+        self.assertEqual('@F28@', element_2.get_pointer())
+        element_2_children = element_2.get_child_elements()
+        self.assertEqual(5, len(element_2_children))
+        self.assertEqual('HUSB', element_2_children[0].get_tag())
+        self.assertEqual('WIFE', element_2_children[1].get_tag())
+        self.assertEqual('CHIL', element_2_children[2].get_tag())
+        self.assertEqual('@I84@', element_2_children[3].get_value())
