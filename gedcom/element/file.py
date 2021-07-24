3f661fd088d56ee4975b3644b58d8b55317cc495
Patrick Swanson <patswanson@gmail.com>
2021-07-24 13:46:19 -0500

    Remove get_tag method from FileElement - the method didn't do anything valuable, and it inherits a similar method from Element.

--- gedcom/element/file.py
+++ gedcom/element/file.py
@@ -1,7 +1,6 @@
 """GEDCOM element consisting of tag `gedcom.tags.GEDCOM_TAG_FILE`"""
 
 from gedcom.element.element import Element
-import gedcom.tags
 
 
 class NotAnActualFileError(Exception):
@@ -9,6 +8,4 @@ class NotAnActualFileError(Exception):
 
 
 class FileElement(Element):
-
-    def get_tag(self):
-        return gedcom.tags.GEDCOM_TAG_FILE
+    pass
