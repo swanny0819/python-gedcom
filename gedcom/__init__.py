41b6b7953ed7bceed264bf9559f310c9810b6b21
Patrick Swanson <patswanson@gmail.com>
2021-07-26 09:08:45 -0500

    Fix #5: Cannot run pdoc to generate new documentation
    The code in the init files was causing pdoc to explode. Removing the code (but leaving the comments, since they get pulled into the documentation) fixed the issue. I've also generated new documentation and included it.

--- gedcom/__init__.py
+++ gedcom/__init__.py
@@ -3,12 +3,3 @@ A Python module for parsing, analyzing, and manipulating GEDCOM files.
 
 .. include:: ./gedcom.md
 """
-
-__all__ = [
-    # Subpackages
-    "element",
-    # Modules
-    "helpers",
-    "parser",
-    "tags"
-]
